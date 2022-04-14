7b9625b30cd6e882f2ec79d90dc9e97a650f63f7
Hobby-Dev // <72003733+Hobby-Dev-0@users.noreply.github.com>
2022-04-14 12:38:38 +0530

    BaseStart

---
+++ X/main/start_utility.py
@@ -0,0 +1,40 @@
+from .apivar import *
+import os
+import sys
+import time
+from distutils.util import strtobool as sb
+from logging import DEBUG, INFO, basicConfig, getLogger
+import heroku3
+from dotenv import load_dotenv
+from requests import get
+StartTime = time.time()
+
+
+CONSOLE_LOGGER_VERBOSE = sb(union("CONSOLE_LOGGER_VERBOSE", "False"))
+
+
+if CONSOLE_LOGGER_VERBOSE:
+    basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=DEBUG,
+    )
+else:
+    basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                level=INFO)
+    
+LOGS = getLogger("X")
+
+# global variables
+CMD_LIST = {}
+CMD_HELP = {}
+CMD_HELP_BOT = {}
+BRAIN_CHECKER = []
+INT_PLUG = ""
+LOAD_PLUG = {}
+COUNT_MSG = 0
+USERS = {}
+COUNT_PM = {}
+LASTMSG = {}
+ISAFK = False
+AFKREASON = None
+SUDO_LIST = {}
