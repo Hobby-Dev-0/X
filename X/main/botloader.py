606621ad26d29c1dbd9dd3e8b5b278da2b61b060
Hobby-Dev // <72003733+Hobby-Dev-0@users.noreply.github.com>
2022-04-15 09:15:58 +0530

    botloader

---
+++ X/main/botloader.py
@@ -0,0 +1,22 @@
+from .botstart import BaseClient
+from sys import argv
+import sys
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+import os
+from telethon import TelegramClient
+from ..utility.utils import load_utils
+from .start_utility import LOAD_PLUG, BOTLOG_CHATID, LOGS
+from pathlib import Path
+import asyncio
+import telethon.utils
+
+
+async def modules_loading():
+  import glob
+  path = 'X/plugins/*.py'
+  files = glob.glob(path)
+  for name in files:
+    with open(name) as f:
+      path1 = Path(f.name)
+      shortname = path1.stem
+      load_utils(shortname.replace(".py", ""))
