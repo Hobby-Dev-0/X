1d89e86f2df8ffb26e9dfafa9d18c3c3b0f80ee5
Hobby-Dev // <72003733+Hobby-Dev-0@users.noreply.github.com>
2022-04-15 09:52:29 +0530

    New Try

---
+++ X/plugins/hello.py
@@ -0,0 +1,11 @@
+from . import *
+
+@BaseClient.on(X_cmd(pattern=("Hi"))
+async def _(aman):
+    if aman.fwd_from:
+        return
+    try:
+        await aman.edit(chat_id, "Hello! My Friend")
+    except:
+        await aman.delete()
+        await BaaseClient.send_message(chat_id, "Hello! My Friend")
