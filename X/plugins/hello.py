8e0b77bbb2269a542b68652c2104804361b4ce20
Hobby-Dev // <72003733+Hobby-Dev-0@users.noreply.github.com>
2022-04-15 10:44:30 +0530

    try

--- X/plugins/hello.py
+++ X/plugins/hello.py
@@ -5,7 +5,9 @@ async def _(aman):
     if aman.fwd_from:
         return
     try:
-        await aman.edit(chat_id, "Hello! My Friend")
+        await BaseClient.edit_message(aman.chat_id, "Hello! My Friend")
     except:
-        await aman.delete()
-        await BaaseClient.send_message(chat_id, "Hello! My Friend")
+        await BaseClient.send_messagee(aman.chat_id, "Hello! My Friend")
+   # except:
+    #    await aman.delete()
+     #   await BaaseClient.send_message(chat_id, "Hello! My Friend")
