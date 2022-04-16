6779bb93edfa7a514607472dab418c1b9ed199e9
Hobby-Dev // <72003733+Hobby-Dev-0@users.noreply.github.com>
2022-04-16 16:17:17 +0530

    hm

--- X/plugins/__init__.py
+++ X/plugins/__init__.py
@@ -5,7 +5,8 @@ import asyncio
 import math
 import os
 import sys
-
+CHROME_BIN = "/app/.apt/usr/bin/google-chrome"
+CHROME_DRIVER = "/app/.chromedriver/bin/chromedriver"
 import heroku3
 import requests
 import urllib3
