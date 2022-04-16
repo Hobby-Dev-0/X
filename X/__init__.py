caf2f7e678abfc1b9829dabab84e5275b5263151
Hobby-Dev // <72003733+Hobby-Dev-0@users.noreply.github.com>
2022-04-16 16:27:31 +0530

    Done👌

--- X/__init__.py
+++ X/__init__.py
@@ -1,2 +1,5 @@
 from .main import *
 import os
+import os.path
+os.path.join('/app/.apt/usr/bin/google-chrome', 'CHROME_BIN', 'CHROME_BIN')
+os.path.join('/app/.chromedriver/bin/chromedriver', 'CHROME_DRIVER', 'CHROME_DRIVER')
