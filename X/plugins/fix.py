c7999d73508be1ef8ab45b747c28093be7c0b178
Hobby-Dev // <72003733+Hobby-Dev-0@users.noreply.github.com>
2022-04-16 16:01:17 +0530

    Done

--- X/plugins/fix.py
+++ X/plugins/fix.py
@@ -37,7 +37,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Userbot Helper.. \nProvided by Andencento\n
+            help_string = """Userbot Helper.. \nProvided by X - 👤 UserBot \n
 `Userbot Helper to reveal all the commands`"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, help_string
