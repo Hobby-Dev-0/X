1c132a0c29d5039c9c18be58b3ddb0b644712e01
Hobby-Dev // <72003733+Hobby-Dev-0@users.noreply.github.com>
2022-04-17 13:13:39 +0530

    @Client Modifcation

--- X/__main__.py
+++ X/__main__.py
@@ -21,19 +21,25 @@ if len(argv) not in (1, 3, 4):
 else:
     BaseClient.tgbot = None
     if BOT_USERNAME is not None:
-        print("Speedo USERBOT STABLE VERSION STARTING")
+        print("STARTING - X")
         BaseClient.tgbot = TelegramClient(
             "TG_BOT_TOKEN",
             api_id=API_ID,
             api_hash=API_HASH
         ).start(bot_token=BOT_TOKEN)
-        print("X Started!")
+        print("X - STARTED!")
         BaseClient.loop.run_until_complete(add_bot(BOT_USERNAME))
-        print("Loading Software Completed")
+        print("Loading Modules")
     else:
         BaseClient.start()
         
 BaseClient.loop.run_until_complete(modules_loading())
+print("All Modules Loaded Successfully!")
+
+print(""" Thanks For Deploying our userbot X
+                Free to Contact us on telegram @X_UB_SUPPORT 
+                        Hope So You like our userbot
+     """)
         
 if len(argv) not in (1, 3, 4):
     BaseClient.disconnect()
