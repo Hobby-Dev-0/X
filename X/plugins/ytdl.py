24618011e5eea44ba5d0487f17c12efa86a1eed0
Hobby-Dev // <72003733+Hobby-Dev-0@users.noreply.github.com>
2022-04-21 15:12:38 +0530

    ytdl

--- X/plugins/ytdl.py
+++ X/plugins/ytdl.py
@@ -3,161 +3,211 @@ import json
 import os
 import time
 os.system("pip install -U youtube_dl && pip install -U youtube_search && pip install -U youtube-search-python && pip install -U youtube-search")
-from telethon.tl.types import DocumentAttributeAudio
+
+import os
+import time
+import math
+import asyncio
 from youtube_dl import YoutubeDL
-from youtube_dl.utils import (ContentTooShortError, DownloadError,
+from youtube_dl.utils import (DownloadError, ContentTooShortError,
                               ExtractorError, GeoRestrictedError,
                               MaxDownloadsReached, PostProcessingError,
                               UnavailableVideoError, XAttrMetadataError)
-from youtube_search import YoutubeSearch
-
+from asyncio import sleep
+from telethon.tl.types import DocumentAttributeAudio
 from . import *
 
 
-@BaseClient.on(X_cmd(pattern="yt(a|v)"))
+async def progress(current, total, event, start, type_of_ps, file_name=None):
+    """Generic progress_callback for uploads and downloads."""
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        progress_str = "{0}{1} {2}%\n".format(
+            ''.join(["▰" for i in range(math.floor(percentage / 10))]),
+            ''.join(["▱" for i in range(10 - math.floor(percentage / 10))]),
+            round(percentage, 2))
+        tmp = progress_str + \
+            "{0} of {1}\nETA: {2}".format(
+                humanbytes(current),
+                humanbytes(total),
+                time_formatter(estimated_total_time)
+            )
+        if file_name:
+            await event.edit("{}\nFile Name: `{}`\n{}".format(
+                type_of_ps, file_name, tmp))
+        else:
+            await event.edit("{}\n{}".format(type_of_ps, tmp))
+
+
+def humanbytes(size):
+    """Input size in bytes,
+    outputs in a human readable format"""
+    # https://stackoverflow.com/a/49361727/4723940
+    if not size:
+        return ""
+    # 2 ** 10 = 1024
+    power = 2**10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
+def time_formatter(milliseconds: int) -> str:
+    """Inputs time in milliseconds, to get beautified time,
+    as string"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + " day(s), ") if days else "") + \
+        ((str(hours) + " hour(s), ") if hours else "") + \
+        ((str(minutes) + " minute(s), ") if minutes else "") + \
+        ((str(seconds) + " second(s), ") if seconds else "") + \
+        ((str(milliseconds) + " millisecond(s), ") if milliseconds else "")
+    return tmp[:-2]
+
+@BaseClient.on(X_cmd(pattern="yt(a|v) (.*)"))
 async def download_video(v_url):
-    if v_url.fwd_from:
-        return
-    url = v_url.pattern_match.group(1)
+    """ For .ytdl command, download media from YouTube and many other sites. """
+    url = v_url.pattern_match.group(2)
     type = v_url.pattern_match.group(1).lower()
 
-    event = await eor(v_url, "`Preparing to download...`")
+    await v_url.edit("`Preparing to download...`")
 
     if type == "a":
         opts = {
-            "format": "bestaudio",
-            "addmetadata": True,
-            "key": "FFmpegMetadata",
-            "writethumbnail": True,
-            "prefer_ffmpeg": True,
-            "geo_bypass": True,
-            "nocheckcertificate": True,
-            "postprocessors": [
-                {
-                    "key": "FFmpegExtractAudio",
-                    "preferredcodec": "mp3",
-                    "preferredquality": "480",
-                }
-            ],
-            "outtmpl": "%(id)s.mp3",
-            "quiet": True,
-            "logtostderr": False,
+            'format':
+            'bestaudio',
+            'addmetadata':
+            True,
+            'key':
+            'FFmpegMetadata',
+            'writethumbnail':
+            True,
+            'prefer_ffmpeg':
+            True,
+            'geo_bypass':
+            True,
+            'nocheckcertificate':
+            True,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '480',
+            }],
+            'outtmpl':
+            '%(id)s.mp3',
+            'quiet':
+            True,
+            'logtostderr':
+            False
         }
         video = False
         song = True
 
     elif type == "v":
         opts = {
-            "format": "best",
-            "addmetadata": True,
-            "key": "FFmpegMetadata",
-            "prefer_ffmpeg": True,
-            "geo_bypass": True,
-            "nocheckcertificate": True,
-            "postprocessors": [
-                {"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}
-            ],
-            "outtmpl": "%(id)s.mp4",
-            "logtostderr": False,
-            "quiet": True,
+            'format':
+            'best',
+            'addmetadata':
+            True,
+            'key':
+            'FFmpegMetadata',
+            'prefer_ffmpeg':
+            True,
+            'geo_bypass':
+            True,
+            'nocheckcertificate':
+            True,
+            'postprocessors': [{
+                'key': 'FFmpegVideoConvertor',
+                'preferedformat': 'mp4'
+            }],
+            'outtmpl':
+            '%(id)s.mp4',
+            'logtostderr':
+            False,
+            'quiet':
+            True
         }
         song = False
         video = True
 
     try:
-        await event.edit("**Fetching YT link...**")
+        await v_url.edit("`Fetching data, please wait..`")
         with YoutubeDL(opts) as ytdl:
             ytdl_data = ytdl.extract_info(url)
     except DownloadError as DE:
-        await edit_or_reply(v_url, f"`{str(DE)}`")
+        await v_url.edit(f"`{str(DE)}`")
         return
     except ContentTooShortError:
-        await eod(event, "`The download content was too short.`")
+        await v_url.edit("`The download content was too short.`")
         return
     except GeoRestrictedError:
-        await eod(
-            event,
-            "`Video is not available from your geographic location due to geographic restrictions imposed by a website.`",
+        await v_url.edit(
+            "`Video is not available from your geographic location due to geographic restrictions imposed by a website.`"
         )
         return
     except MaxDownloadsReached:
-        await eod(event, "`Max-downloads limit has been reached.`")
+        await v_url.edit("`Max-downloads limit has been reached.`")
         return
     except PostProcessingError:
-        await eod(event, "`There was an error during post processing.`")
+        await v_url.edit("`There was an error during post processing.`")
         return
     except UnavailableVideoError:
-        await eod(event, "`Media is not available in the requested format.`")
+        await v_url.edit("`Media is not available in the requested format.`")
         return
     except XAttrMetadataError as XAME:
-        await edit_or_reply(v_url, f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
+        await v_url.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
         return
     except ExtractorError:
-        await eod(event, "`There was an error during info extraction.`")
+        await v_url.edit("`There was an error during info extraction.`")
         return
     except Exception as e:
-        await eod(event, f"{str(type(e)): {str(e)}}", 10)
+        await v_url.edit(f"{str(type(e)): {str(e)}}")
         return
     c_time = time.time()
     if song:
-        await eor(
-            event,
-            f"📤 `Preparing to upload audio:`\
-        \n\n**{ytdl_data['title']}**\
-        \nby *{ytdl_data['uploader']}*",
-        )
+        await v_url.edit(f"`Preparing to upload song:`\
+        \n**{ytdl_data['title']}**\
+        \nby *{ytdl_data['uploader']}*")
         await v_url.client.send_file(
             v_url.chat_id,
             f"{ytdl_data['id']}.mp3",
             supports_streaming=True,
             attributes=[
-                DocumentAttributeAudio(
-                    duration=int(ytdl_data["duration"]),
-                    title=str(ytdl_data["title"]),
-                    performer=perf,
-                )
+                DocumentAttributeAudio(duration=int(ytdl_data['duration']),
+                                       title=str(ytdl_data['title']),
+                                       performer=str(ytdl_data['uploader']))
             ],
-            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                progress(
-                    d, t, v_url, c_time, "Uploading..", f"{ytdl_data['title']}.mp3"
-                )
-            ),
-        )
+            progress_callback=lambda d, t: asyncio.get_event_loop(
+            ).create_task(
+                progress(d, t, v_url, c_time, "Uploading..",
+                         f"{ytdl_data['title']}.mp3")))
         os.remove(f"{ytdl_data['id']}.mp3")
         await v_url.delete()
     elif video:
-        await eor(
-            event,
-            f"`Preparing to upload video:`\
-        \n\n**{ytdl_data['title']}**\
-        \nby *{ytdl_data['uploader']}*",
-        )
+        await v_url.edit(f"`Preparing to upload video:`\
+        \n**{ytdl_data['title']}**\
+        \nby *{ytdl_data['uploader']}*")
         await v_url.client.send_file(
             v_url.chat_id,
             f"{ytdl_data['id']}.mp4",
             supports_streaming=True,
-            caption=ytdl_data["title"],
-            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                progress(
-                    d, t, v_url, c_time, "Uploading..", f"{ytdl_data['title']}.mp4"
-                )
-            ),
-        )
+            caption=ytdl_data['title'],
+            progress_callback=lambda d, t: asyncio.get_event_loop(
+            ).create_task(
+                progress(d, t, v_url, c_time, "Uploading..",
+                         f"{ytdl_data['title']}.mp4")))
         os.remove(f"{ytdl_data['id']}.mp4")
         await v_url.delete()
-
-
-@BaseClient.on(X_cmd(pattern="ytlink"))
-async def hmm(ytwala):
-    query = ytwala.pattern_match.group(2)
-    if not query:
-        await eod(ytwala, "`Enter query to search on yt`")
-    event = await eor(ytwala, "`Processing...`")
-    try:
-        results = json.loads(YoutubeSearch(query, max_results=7).to_json())
-    except KeyError:
-        return await eod(event, "Unable to find relevant search queries...")
-    output = f"**Search Query:**\n`{query}`\n\n**Results:**\n\n"
-    for i in results["videos"]:
-        output += f"--> `{i['title']}`\nhttps://www.youtube.com{i['url_suffix']}\n\n"
-    await event.edit(output, link_preview=False)
+        
